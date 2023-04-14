c458cfd677698942e24f93bbcae0bf52c3d2f335
Franjhe <franjhely.andre13@gmail.com>
2023-04-14 12:47:41 -0400

    display of service requests in the club agenda

--- src/app/club/dashboard/dashboard.component.ts
+++ src/app/club/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, UntypedFormGroup, Validators,FormControl, FormGroup, AbstractControl } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, ChangeDetectorRef , OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from '@services/authentication.service';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction'; // for selectable
+import dayGridPlugin from '@fullcalendar/daygrid'; // fo
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,26 +15,105 @@ import { AuthenticationService } from '@services/authentication.service';
 })
 
 export class DashboardComponent implements OnInit {
-
-
-  User : FormGroup
-  submitted = false;
+  savedatacalendar = false
+  code
+  Events: any[] = [];
+  calendarVisible = true;
+  calendarOptions: CalendarOptions = {
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'dayGridMonth,dayGridWeek,dayGridDay'
+    },
+    initialView: 'dayGridMonth',
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this)
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+  };
+  currentEvents: EventApi[] = [];
   message : any;
   currentUser;
-  DataTypeService : any[] = [];
-  DataService : any[] = [];
-
-
   constructor(
-    private formBuilder: FormBuilder,
     private authenticationService : AuthenticationService,
     private http : HttpClient,
-    private router : Router
+    private changeDetector: ChangeDetectorRef
   ) { }
 
-  ngOnInit() {
-
-
+  handleCalendarToggle() {
+    this.calendarVisible = !this.calendarVisible;
   }
+  handleWeekendsToggle() {
+    const { calendarOptions } = this;
+    calendarOptions.weekends = !calendarOptions.weekends;
+  }
+  handleDateSelect(selectInfo: DateSelectArg) {
+    this.savedatacalendar = true
+    const title = prompt('¿Qué actividad planea realizar?');
+    const calendarApi = selectInfo.view.calendar;
+    this.currentUser = this.authenticationService.currentUserValue;
+    let params = {
+      title: title,
+      start: selectInfo.startStr,
+      end: selectInfo.endStr,
+      allDay: selectInfo.allDay,
+      cpropietario: this.currentUser.data.cpropietario
+    }
+    this.http
+    .post(environment.apiUrl + '/api/club/client-agenda' ,params)
+    .subscribe((res: any) => {
+      this.Events = []
+      this.Events = res.data.list
+      this.calendarOptions = {
+        initialView: 'dayGridMonth',
+        events: this.Events,
+      };
+    });
+  }
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`¿Seguro de que deseas elminar '${clickInfo.event.title}'?`)) {
+      clickInfo.event.remove();
+    }
+  }
+  handleEvents(events: EventApi[]) {
+    this.currentEvents = events;
+    this.changeDetector.detectChanges();
+  }
+  ngOnInit(){
+    this.currentUser = this.authenticationService.currentUserValue;
+    let params = {
+      cpropietario: this.currentUser.data.cpropietario
+    }
+    this.http
+    .post(environment.apiUrl + '/api/club/search/client-agenda' ,params)
+    .subscribe((res: any) => {
+      console.log(res)
+      const datagend = res.data.datagenda
+      const dataevent = res.data.list
+      const data = datagend.contact(dataevent)
+      this.Events = data
+      console.log(this.Events)
+      this.calendarOptions = {
+        initialView: 'dayGridMonth',
+        events: this.Events,
+      };
+    });    
+  }
+
 
 }
--- src/app/club/layouts/admin-layout/admin-layout.module.ts
+++ src/app/club/layouts/admin-layout/admin-layout.module.ts
@@ -13,6 +13,8 @@ import { MatRippleModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSelectModule } from '@angular/material/select';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
 
 
 @NgModule({
@@ -27,6 +29,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatSelectModule,
     MatTooltipModule,
+    FullCalendarModule
   ],
   declarations: [
     DashboardComponent,
