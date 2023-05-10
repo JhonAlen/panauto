c3d20a5e062adeef2a48f6e07380e47acbb88e7e
Franjhe <franjhely.andre13@gmail.com>
2023-05-10 11:23:16 -0400

    implementation of the maintenance management charge in club calendars

--- src/app/club/dashboard/dashboard.component.ts
+++ src/app/club/dashboard/dashboard.component.ts
@@ -21,18 +21,24 @@ import { FormBuilder, UntypedFormGroup, Validators,FormControl, FormGroup, Abstr
 export class DashboardComponent implements OnInit {
   @ViewChild("content") private contentRef: TemplateRef<Object>;
   @ViewChild("calendar") private content: TemplateRef<Object>;
+  @ViewChild("documents") public contentDocument: TemplateRef<Object>;
+  @ViewChild("mantenimiento") public contentMantenimiento: TemplateRef<Object>;
+
+  DataUserMantenimiento: FormGroup
   EventsCalendar : FormGroup
+  DataUserDocuments : FormGroup
 
   closeResult: string;
-
   name : string
   apellido: string
   date : any
-  message = false;
+  message :any ;
+  xrutaarchivo : string
+  submitted = false;
   datamessage : any
 
-  code
   Events: any[] = [];
+
   calendarVisible = true;
   calendarOptions: CalendarOptions = {
     plugins: [
@@ -96,6 +102,8 @@ export class DashboardComponent implements OnInit {
       this.http
       .post(environment.apiUrl + '/api/club/client-agenda' ,params)
       .subscribe((res: any) => {
+        console.log(res.data.list)
+
         this.Events = []
         this.Events = res.data.list
         this.calendarOptions = {
@@ -127,6 +135,25 @@ export class DashboardComponent implements OnInit {
       fhasta:  [''],
     });
 
+    this.DataUserDocuments = this.formBuilder.group({
+      cpropietario:  [''],
+      xdocumento:  [''],
+      xarchivo:  [''],
+      itipodocumento:  [''],
+      fvencimiento:  [''],
+      fcreacion :  [''],
+      cusuariocreacion:  [''],
+
+    });
+
+    this.DataUserMantenimiento = this.formBuilder.group({
+      fdesde :  [''],
+      xmantenimientoPrevent:  [''],
+      hora:  [''],
+      xmantenimientoCorrect:  [''],
+
+    });
+
     this.currentUser = this.authenticationService.currentUserValue;
     let params = {
       cpropietario: this.currentUser.data.cpropietario
@@ -156,4 +183,76 @@ export class DashboardComponent implements OnInit {
     },);
   }
 
+  openDocument(contentDocument){
+    this.modalService.open(contentDocument, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    },);
+  }
+
+  openMantenimiento(contentMantenimiento){
+    this.modalService.open(contentMantenimiento, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    },);
+  }
+
+  onFileSelect(event){
+    const file = event.target.files[0];
+    this.DataUserDocuments.get('xarchivo').setValue(file);
+    this.submitted=true
+
+  }
+
+
+  onSubmit(form){
+    const formData = new FormData();
+    formData.append('xdocumento',this.DataUserDocuments.get('xarchivo').value);
+    formData.append('agentId', '007');
+    this.http.post<any>(`${environment.apiUrl}/api/upload/document`, formData).subscribe(response => {
+      if(response.data.status){
+        this.xrutaarchivo = `${environment.apiUrl}/documents/${response.data.uploadedFile.filename}`;
+        
+        let params = {
+          cpropietario: this.currentUser.data.cpropietario,
+          xarchivo: this.xrutaarchivo,
+          itipodocumento: form.itipodocumento,
+          fvencimiento: form.fvencimiento,
+          cusuariocreacion: this.currentUser.data.cusuario,
+        }
+
+        this.http.post<any>(environment.apiUrl + `/api/club/upload/client-agenda`, params).subscribe(response => {
+          if(response.data.status){
+            this.DataUserDocuments.reset()
+            this.message = 'Documento guardado con éxito'
+            this.ngOnInit()
+          }
+        })
+
+        
+      }
+    })
+    
+  }
+
+  onSubmitMantenimiento(form){
+
+    let data ={
+      cpropietario : this.currentUser.data.cpropietario,
+      fdesde : form.fdesde,
+      xmantenimientoPrevent : form.xmantenimientoPrevent,
+      hora : form.hora,
+      xmantenimientoCorrect : form.xmantenimientoCorrect,
+    } 
+
+    this.http.post<any>(environment.apiUrl + `/api/club//upload/mantenimiento/client-agenda`, data).subscribe(response => {
+      if(response.data.status){
+        this.DataUserMantenimiento.reset()
+        this.message = 'Mantenimiento con éxito'
+        this.ngOnInit()
+      }
+    })
+
+        
+      
+    
+  }
 }
